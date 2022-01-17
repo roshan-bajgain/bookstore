7797c4202c3e1672979ef925bda8a8830bdc479c
Roshan Bajgain <you@example.com>
2022-01-17 21:05:39 +0545

    Add router and route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Books from './components/books';
 import nav from './components/nav'
 import catagories from './components/categories'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <nav />
+      <Routes>
+      <Route exact path="/" element={<Books />} />
+      <Route exact path="/categories" element={<catagories />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
