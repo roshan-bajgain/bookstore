51b62d0635c942fa79925a672b5e3cb17537d343
Roshan Bajgain <you@example.com>
2022-01-17 19:26:02 +0545

    Add form in addBooks.js file

---
+++ src/components/addBooks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddBooks = () => (
+  <form>
+    <input placeholder='ADD NEW Book' />
+    <select name='books' id='book'>
+        <option value='science'>Friction</option>
+        <option value='Action'>volvo</option>
+        <option value='Economic'>money</option>
+        <option value='Account'>credit</option>
+    </select>
+    <button type='submit'>Add Book</button>
+  </form>
+)
+
+export default AddBooks
