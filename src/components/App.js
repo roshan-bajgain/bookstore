14ca3fb9357923d9f8ea479420888ee4ad393efe
Roshan Bajgain <you@example.com>
2022-01-17 22:53:25 +0545

    Add linter file and fix the error

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Route, BrowserRouter, Routes, Link } from 'react-router-dom';
+import {
+  Route, BrowserRouter, Routes, Link,
+} from 'react-router-dom';
 import BooksPage from '../redux/books/books';
 import CategoriesPage from '../redux/categories/categories';
 
-
 const App = () => (
-    <BrowserRouter>
-      <div className="wrapper-er">
-        <nav className="nav">
-          <h1 className="heading">Math Magicians</h1>
-          <ul className="link">
-            <Link to="/">BOOKS</Link>
-            <Link to="/calculator">CATEGORIES</Link>
-          </ul>
-        </nav>
-      </div>
-      <Routes>
-        <Route path="/" element={<BooksPage/>} />
-        <Route path="/calculator" element={<CategoriesPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-  export default App;
+  <BrowserRouter>
+    <div className="wrapper-er">
+      <nav className="nav">
+        <ul className="link">
+          <Link to="/">BOOKS</Link>
+          <Link to="/calculator">CATEGORIES</Link>
+        </ul>
+      </nav>
+    </div>
+    <Routes>
+      <Route path="/" element={<BooksPage />} />
+      <Route path="/calculator" element={<CategoriesPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
