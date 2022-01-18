07a25c1b08a7568d1430c96fc19b7964e668676e
Roshan Bajgain <you@example.com>
2022-01-18 15:29:22 +0545

    Add variable for Add_book and remove_book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,15 +1,17 @@
-import React from 'react';
+// import React from 'react';
 
-const BooksPage = () => (
-  <div>
-    <div className="books-container d-flex">
-      <h2>Name of Book</h2>
-      <p>The Merchant of Venice</p>
-      <button type="button">Delete</button>
-    </div>
-    <input type="text" name="addBooks" placeholder="Book Title" />
-    <button type="button">Add Books</button>
-  </div>
-);
+// const BooksPage = () => (
+//   <div>
+//     <div className="books-container d-flex">
+//       <h2>Name of Book</h2>
+//       <p>The Merchant of Venice</p>
+//       <button type="button">Delete</button>
+//     </div>
+//     <input type="text" name="addBooks" placeholder="Book Title" />
+//     <button type="button">Add Books</button>
+//   </div>
+// );
 
-export default BooksPage;
+// export default BooksPage;
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
