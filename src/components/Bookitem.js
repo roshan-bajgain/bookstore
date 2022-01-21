8f07f2ec3dcc6b2947953954a3e970c96eb956e5
Roshan Bajgain <you@example.com>
2022-01-21 14:52:48 +0545

    Add styling for the circular progress bar

--- src/components/Bookitem.js
+++ src/components/Bookitem.js
@@ -18,29 +18,29 @@ const BookItem = (props) => {
   const percentage = 67;
 
   return (
-    <div>
       <div className="books-container d-flex">
-        <p className="title">{title}</p>
-        <p className="author">{category}</p>
-        <div className="btns">
-          <button className="btn1" type="button">Comment</button>
-          <button className="btn1" onClick={removeBookFromStore} type="button"> Remove </button>
-          <button className="btn1" type="button">Edit</button>
+        <div className='first-wrapper'>
+          <p className="title">{title}</p>
+          <p className="author">{category}</p>
+          <div className="btns">
+            <button className="btn1" type="button">Comment</button>
+            <button className="btn1" onClick={removeBookFromStore} type="button"> Remove </button>
+            <button className="btn1" type="button">Edit</button>
+          </div>
         </div>
-      </div>
-      <div className="progress-bar-c">
-        <CircularProgressbar
-          value={percentage}
-          styles={buildStyles({ pathColor: '#0EA5E9', marginRight: '10px' })}
-          className="progress-bar"
-        />
-        <div className="progress-value-c">
-          <p className="progress-percentage">
-            {percentage}
-            %
-          </p>
-          <p className="progress-completed">Completed</p>
-        </div>
+        <div className="progress-bar-c">
+          <CircularProgressbar
+            value={percentage}
+            styles={buildStyles({ pathColor: '#0EA5E9', marginRight: '10px' })}
+            className="progress-bar"
+          />
+          <div className="progress-value-c">
+            <p className="progress-percentage">
+              {percentage}
+              %
+            </p>
+            <p className="progress-completed">Completed</p>
+          </div>
       </div>
       <div className="update-progg-c">
         <h2 className="chapter-h2">CURRENT CHAPTER</h2>
@@ -51,7 +51,7 @@ const BookItem = (props) => {
         </h3>
         <button type="button" className="update-progg-btn">Update Progress</button>
       </div>
-    </div>
+      </div>
   );
 };
 BookItem.propTypes = {
